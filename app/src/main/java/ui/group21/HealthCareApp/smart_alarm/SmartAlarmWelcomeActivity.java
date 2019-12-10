05ce2f46036e293c74c0e96f01bdbae16f95a0b5
vukihai <vukihai98@gmail.com>
2019-12-10 15:32:49 +0700

    add welcom silder for smart alarm

---
+++ app/src/main/java/ui/group21/HealthCareApp/smart_alarm/SmartAlarmWelcomeActivity.java
@@ -0,0 +1,14 @@
+package ui.group21.HealthCareApp.smart_alarm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SmartAlarmWelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_smart_alarm_welcome);
+    }
+}
